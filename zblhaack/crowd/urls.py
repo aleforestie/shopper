b8d0d35f823883a8972313f4034ce9867c9bca9a
ghany <ghany@enseirb-matmeca.fr>
2017-05-07 12:10:28 +0200

    ee

--- zblhaack/crowd/urls.py
+++ zblhaack/crowd/urls.py
@@ -3,12 +3,10 @@ from django.core.urlresolvers import reverse_lazy
 from crowd import views
 
 urlpatterns = [
-    url(r'^$', views.HelloView.as_view(), name="hw"),
-    url(r'^test/$', views.TestView.as_view(), name="hw2"),
 
     url(r'^products/$', views.ProductListView.as_view(), name='product-list'),
     url(r'^products/ask/$', views.ProductAskView.as_view(), name='product-ask'),
-    url(r'^products/choose/$', views.ChooseView.as_view(), name='product-choose'),
+    url(r'^$', views.ChooseView.as_view(), name='product-choose'),
     url(r'^products/display/$', views.DisplayView.as_view(), name='product-display'),
 
     url(r'^products/ask/ajaxYes/(?P<pk>[0-9]+)/$',
